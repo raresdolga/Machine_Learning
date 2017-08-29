8c64988fb7750033a62bf283c79639c243f71a0e
raresdolga <rares.dolga.16@ucl.ac.uk>
2017-08-29 17:06:49 +0300

    back propagation complete

--- Digit_Recognition/digits.py
+++ Digit_Recognition/digits.py
@@ -5,27 +5,32 @@ import numpy as np
 def cost(images, lables):
     m = len(lables)
     x = np.ones((m, 1))
-    #add bias unit to the image matix
+    # add bias unit to the image matix
     images = np.column_stack((x, images))
-    #initialize theta for 2 hidden layers
-    theta1 = np.zeros((785, 160))
-    theta2 = np.zeros((161, 10))
-    #hidden layers will be denoted by a1,a2 ..
+    # initialize theta for 2 hidden layers
+    theta1 = np.random.rand(785, 160)
+    theta2 = np.random.rand(161, 10)
+    # hidden layers will be denoted by a1,a2 ...
+    # a1 is the first hidden layer ( i.e the second layer in the neuronal network
     a1 = sigmoid(np.dot(images, theta1))
     a1 = np.column_stack((x, a1))
     a2 = sigmoid(np.dot(a1, theta2))
 
     y = format_lables(lables)
     J = (1/m)*(np.sum(log_help(a2, y)))
-    d1 = np.zeros(np.shape(theta1));
-    d2 = np.zeros(np.shape(theta2));
+    # this are partial derivatives
+    d1 = np.zeros(np.shape(theta1))
+    d2 = np.zeros(np.shape(theta2))
 
     # back propagation for batch/stochastic gradient
-
+    ones = np.ones(np.shape(a1))
     for i in range(m):
-        delta3 = y[i,:] - a2[i,:]
-        print (np.shape(delta3))
-        delta2 = (np.dot(theta2, delta3))
+        # eliminate the bias unit
+        delta3 = np.matrix(y[i, :] - a2[i, :])
+        delta2 = np.multiply((np.dot(delta3, np.transpose(theta2[1:, :]))), np.multiply(a1[:, 1:], (ones - a1)[:, 1:]))
+        d1 += np.dot(np.transpose(images),delta2)
+        d2 +=  np.transpose(np.multiply(np.transpose(delta3), np.matrix(a1[i, :])))
+
 
 def sigmoid(z):
     g = 1.0/(1.0+np.exp(-z))
